7ea1b008d8314743d4e730a515adc48dfa9be383
Shubham Kumar <shubhamkumar-mac@Shubhams-MacBook-Air.local>
2026-03-10 20:05:10 +0530

    Add Structure to Site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkPassword(event) {
     event.preventDefault(); // Stop the default link action
     const password = prompt("Enter password:");
-    if (password === "mypassword") {
+    if (password === "xxxsite") {
         // Redirect to the link's href
         window.location.href = event.currentTarget.getAttribute("href");
     } else {
