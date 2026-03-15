6db420afff630bbd204cf0de69956ed4ff635a5b
Shubham Kumar <shubhamkumar-mac@Shubhams-MacBook-Air.local>
2026-03-15 23:34:51 +0530

    Add password gate to study.html

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function checkPassword(event) {
         alert("Wrong password!");
     }
 }
+
+let gate = document.querySelector("#gate")
+gate.addEventListener("click", gtentry)
+function gtentry(){
+    const pass = "kaushik"
+    const pwd = prompt("Enter Password to Proceed")
+    if (pwd === pass){
+        window.location.href = "./study.html"
+    }
+    else {
+        alert("Wrong password!");
+    }
+}
